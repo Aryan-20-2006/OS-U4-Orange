4373e3aaf1115224623fba3798d0ae9c4ef11a6b
LOSt-GENZ <daringdumplings@gmail.com>
2026-04-17 22:57:24 +0530

    phase2: validate tree_from_index output pointer

--- tree.c
+++ tree.c
@@ -200,6 +200,7 @@ static int write_tree_level(const Index *index, const char *prefix, ObjectID *id
 }
 
 int tree_from_index(ObjectID *id_out) {
+    if (!id_out) return -1;
     if (!index_load) return -1;
     Index index;
     if (index_load(&index) != 0) return -1;
